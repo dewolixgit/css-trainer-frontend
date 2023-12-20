6e11fac572e54c124464f5408183d810f03dde55
d.ragozin <dmtryragozin@gmail.com>
2023-12-20 11:23:22 +0300

    fix: Импорт стилей

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
 
+import { Text } from 'components/Text';
+import { FontWeightEnum } from 'entities/fonts';
+import { SizeEnum } from 'entities/size';
+
 const Auth: React.FC = () => {
-  return <div>Авторизация</div>;
+  return (
+    <div>
+      <Text tag="h1" weight={FontWeightEnum.medium} size={SizeEnum.m}>
+        Авторизация
+      </Text>
+    </div>
+  );
 };
 
 export default Auth;
